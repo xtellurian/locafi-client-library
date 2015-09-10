0daad27f87011558ff88799242996c9036b91f70
Rian Finnegan <rfinnegan@ramp.com.au>
2015-09-10 16:45:41 +1000

    removed unused usings

--- Locafi.Client.Model/Query/QueryStrings.cs
+++ Locafi.Client.Model/Query/QueryStrings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Locafi.Client.Model.Query
+﻿namespace Locafi.Client.Model.Query
 {
     public static class QueryStrings
     {
