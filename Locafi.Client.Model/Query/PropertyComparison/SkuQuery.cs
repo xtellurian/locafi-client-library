1dbc515fd83c099baacacb84e40c25be392cfa13
Rian Finnegan <rfinnegan@ramp.com.au>
2015-09-14 16:38:14 +1000

    removed usings? not sure whats going on here,.,

--- Locafi.Client.Model/Query/PropertyComparison/SkuQuery.cs
+++ Locafi.Client.Model/Query/PropertyComparison/SkuQuery.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Locafi.Client.Model.Dto.Skus;
+﻿using Locafi.Client.Model.Dto.Skus;
 
 namespace Locafi.Client.Model.Query.PropertyComparison
 {
