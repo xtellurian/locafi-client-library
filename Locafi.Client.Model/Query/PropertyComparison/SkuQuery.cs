473a4c00e052e71c8651bcb5e80accdc773272aa
Rian Finnegan <rfinnegan@ramp.com.au>
2015-09-25 14:30:08 +1000

    added static newquery method to SkuQuery

--- Locafi.Client.Model/Query/PropertyComparison/SkuQuery.cs
+++ Locafi.Client.Model/Query/PropertyComparison/SkuQuery.cs
@@ -1,8 +1,17 @@
-﻿using Locafi.Client.Model.Dto.Skus;
+﻿using System;
+using System.Linq.Expressions;
+using Locafi.Client.Model.Dto.Skus;
 
 namespace Locafi.Client.Model.Query.PropertyComparison
 {
     public class SkuQuery : PropertyQueryBase<SkuSummaryDto>
     {
+        public static SkuQuery NewQuery<TProperty>(Expression<Func<SkuSummaryDto, TProperty>> propertyLambda,
+            TProperty value, ComparisonOperator op, int take = 100, int skip = 0)
+        {
+            var query = new SkuQuery();
+            query.CreateQuery(propertyLambda, value, op, take, skip);
+            return query;
+        }
     }
 }
