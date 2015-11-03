686adf13cac4b6176e4213dc85c45acb2b64d218
Rian Finnegan <rfinnegan@ramp.com.au>
2015-11-03 16:11:36 +1100

    added flags to order actions, since multiple are possible

--- Locafi.Client.Model/Enums/Actions/OrderAction.cs
+++ Locafi.Client.Model/Enums/Actions/OrderAction.cs
@@ -1,13 +1,17 @@
-﻿namespace Locafi.Client.Model.Enums.Actions
+﻿using System;
+
+namespace Locafi.Client.Model.Enums.Actions
 {
+    [Flags]
     public enum OrderAction
     {
-        Create,
-        Allocate, // ie, when a client is scanning tags
-        SubmitAllocate,  // ie when a client uploads a snapshot
-        Dispatch,
-        Receive,
-        SubmitReceive,
-        Cancel
+        None = 0,
+        Create = 1,
+        Allocate = 2, // ie, when a client is scanning tags
+        SubmitAllocate = 4,  // ie when a client uploads a snapshot
+        Dispatch = 8,
+        Receive = 16,
+        SubmitReceive = 32,
+        Cancel = 64
     }
 }
