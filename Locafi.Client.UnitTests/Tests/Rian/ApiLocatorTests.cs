f80fa2e9b402b7c623223008a65f887609d29a5e
Rian Finnegan <rfinnegan@ramp.com.au>
2015-11-04 15:59:57 +1100

    started with getting Api Urls, but realised they need to be named

---
+++ Locafi.Client.UnitTests/Tests/Rian/ApiLocatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Locafi.Client.UnitTests.Tests.Rian
+{
+    [TestClass]
+    public class ApiLocatorTests
+    {
+        [TestMethod]
+        public async Task ApiLocator_GetBaseUri()
+        {
+            var result = await LocafiApiLocator.GetApiBaseUrl("something"); // at the moment, there are no usernames, it all returns the same;
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<string>));
+            Assert.IsTrue(result.Count > 0); // we got at least one URI
+        }
+    }
+}
---
+++ Locafi.Client/LocafiApiLocator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Locafi.Client
+{
+    public static class LocafiApiLocator
+    {
+        private const string ApiLocatorUrl =
+            @"https://microsoft-apiapp9db5d7eaeefa415a9a58fc01c3227e14.azurewebsites.net/api/apilocation";
+
+        public static async Task<IList<string>> GetApiBaseUrl(string userName)
+        {
+            var path = $"{ApiLocatorUrl}?userName={userName}";
+            var message = new HttpRequestMessage(HttpMethod.Get, path);
+
+            var client = new HttpClient();
+            var response = await client.SendAsync(message);
+            var serverMessage = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<IList<string>>(serverMessage);
+        }
+    }
+}
