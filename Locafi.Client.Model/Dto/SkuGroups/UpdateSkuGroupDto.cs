07f9fc8a2563a1dc22d7188b1052df8fc7a529e5
Rian Finnegan <rfinnegan@ramp.com.au>
2015-11-20 10:25:02 +1100

    sku group tests now working

--- Locafi.Client.Model/Dto/SkuGroups/UpdateSkuGroupDto.cs
+++ Locafi.Client.Model/Dto/SkuGroups/UpdateSkuGroupDto.cs
@@ -3,16 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Locafi.Client.Model.Dto.Places;
+using Locafi.Client.Model.Dto.Skus;
 
 namespace Locafi.Client.Model.Dto.SkuGroups
 {
     public class UpdateSkuGroupDto
     {
+        public UpdateSkuGroupDto()
+        {
+            
+        }
+
+        public UpdateSkuGroupDto(Guid skuGroupId, Guid? skuGroupNameId = null)
+        {
+            SkuGroupId = skuGroupId;
+            SkuGroupNameId = skuGroupNameId;
+        }
+
+        #region Properties
         public Guid SkuGroupId { get; set; }    // Id of the sku group to modify
         public Guid? SkuGroupNameId { get; set; }    // set if you want to change the name of the group
         public IList<Guid> AddSkuIds { get; set; }
         public IList<Guid> AddPlaceIds { get; set; }
         public IList<Guid> RemoveSkuIds { get; set; }
         public IList<Guid> RemovePlaceIds { get; set; }
+        #endregion
+
+        #region Methods
+        public void AddSku(Guid skuId)
+        {
+            if(AddSkuIds==null) AddSkuIds = new List<Guid>();
+            if (AddSkuIds.Contains(skuId)) return;
+            AddSkuIds.Add(skuId);
+        }
+
+        public void AddSkus(IEnumerable<SkuSummaryDto> skuSummaries)
+        {
+            foreach (var sku in skuSummaries)
+            {
+                AddSku(sku.Id);
+            }
+        }
+
+        public void RemoveSku(Guid skuId)
+        {
+            if(RemoveSkuIds==null) RemoveSkuIds = new List<Guid>();
+            if (RemoveSkuIds.Contains(skuId)) return;
+            RemoveSkuIds.Add(skuId);
+        }
+
+        public void RemoveSkus(IEnumerable<SkuSummaryDto> skuSummaries)
+        {
+            foreach (var sku in skuSummaries)
+            {
+                RemoveSku(sku.Id);
+            }
+        }
+
+        public void AddPlace(Guid placeId)
+        {
+            if(AddPlaceIds==null) AddPlaceIds = new List<Guid>();
+            if (AddPlaceIds.Contains(placeId)) return;
+            AddPlaceIds.Add(placeId);
+        }
+
+        public void AddPlaces(IEnumerable<PlaceSummaryDto> placeSummaries)
+        {
+            foreach (var place in placeSummaries)
+            {
+                AddPlace(place.Id);
+            }
+        }
+
+        public void RemovePlace(Guid placeId)
+        {
+            if(RemovePlaceIds==null) RemovePlaceIds = new List<Guid>();
+            if(RemovePlaceIds.Contains(placeId)) return;
+            RemovePlaceIds.Add(placeId);
+        }
+
+        public void RemovePlaces(IEnumerable<PlaceSummaryDto> places)
+        {
+            foreach (var place in places)
+            {
+                RemovePlace(place.Id);
+            }
+        }
+#endregion
     }
 }
--- Locafi.Client.UnitTests/Tests/Rian/SkuGroupTests.cs
+++ Locafi.Client.UnitTests/Tests/Rian/SkuGroupTests.cs
@@ -11,21 +11,22 @@ namespace Locafi.Client.UnitTests.Tests.Rian
     [TestClass]
     public class SkuGroupTests : WebRepoTestsBase
     {
-        private const string TestGroupName = "Test Group";
-
+        private const string TestGroupName = "First Test Group";
+        private const string SecondTestGroupName = "Second Test Group";
         
         [TestMethod]
         public async Task SkuGroup_CreateDelete()
         {
             var ran = new Random();
-            // first we need a name
+            // first we need 2 group names
             var groupNames =
                 await
                     SkuGroupRepo.QuerySkuGroupNames(SkuGroupNameQuery.NewQuery(g => g.Name, TestGroupName,
                         ComparisonOperator.Equals));
             Assert.IsTrue(groupNames.Entities.Count <= 1, "There should not be multiple of these"); // there should be at most 1 group name like this
             
-            var groupName = groupNames.Entities.FirstOrDefault() ?? await SkuGroupRepo.CreateSkuGroupName(new AddSkuGroupNameDto(TestGroupName)); // create if not exists
+            var groupName1 = groupNames.Entities.FirstOrDefault(n=>string.Equals(n.Name, TestGroupName)) ?? await SkuGroupRepo.CreateSkuGroupName(new AddSkuGroupNameDto(TestGroupName)); // create if not exists
+            var groupName2 = groupNames.Entities.FirstOrDefault(n=>string.Equals(n.Name, SecondTestGroupName)) ?? await SkuGroupRepo.CreateSkuGroupName(new AddSkuGroupNameDto(SecondTestGroupName)); // create if not exists
 
             // get a sku to add
             var skus = await SkuRepo.GetAllSkus();
@@ -35,16 +36,47 @@ namespace Locafi.Client.UnitTests.Tests.Rian
             var places = await PlaceRepo.GetAllPlaces();
             var place = places[ran.Next(places.Count - 1)];
 
-            // now create a new, empty group
-            var addGroupDto = new AddSkuGroupDto(groupName.Id);
+            // now create a new group with 1 place and 1 sku
+            var addGroupDto = new AddSkuGroupDto(groupName1.Id);
             addGroupDto.AddSku(sku.Id);
             addGroupDto.AddPlace(place.Id);
 
             var group = await SkuGroupRepo.CreateSkuGroup(addGroupDto);
-
+            var id = group.Id;
             Assert.IsNotNull(group, "Group was null");
-            Assert.AreEqual(group.SkuGroupName, groupName.Name, "Name of group equals group name");
+            Assert.AreEqual(group.SkuGroupName, groupName1.Name, "Name of group equals group name");
+            Assert.IsTrue(group.Skus.Contains(sku), "Group contains Sku");
+            Assert.IsTrue(group.Places.Contains(place), "Group contains place");
 
+            //get group detail
+            var groupDetail = await SkuGroupRepo.GetSkuGroupDetail(id);
+            Assert.AreEqual(group,groupDetail, "group == groupDetail by Id");
+            Assert.IsTrue(group.Skus.Contains(sku), "GroupDetail contains Sku");
+            Assert.IsTrue(group.Places.Contains(place), "GroupDetail contains place");
+
+            //update group name
+            var updateDto = new UpdateSkuGroupDto(id, groupName2.Id); // change group name to group name 2
+            groupDetail = await SkuGroupRepo.UpdateSkuGroup(id, updateDto);
+            Assert.AreEqual(groupName2.Name, groupDetail.SkuGroupName, "Updated names are equal");
+
+            // remove place and sku, and then re-add them
+            updateDto = new UpdateSkuGroupDto(id);
+            updateDto.RemoveSku(sku.Id);
+            updateDto.RemovePlace(place.Id);
+            groupDetail = await SkuGroupRepo.UpdateSkuGroup(id, updateDto);
+            Assert.IsFalse(groupDetail.Skus.Contains(sku), "Successfully removed sku");
+            Assert.IsFalse(groupDetail.Places.Contains(place), "Succesfully removed place");
+
+            //re-add those places and skus
+            updateDto = new UpdateSkuGroupDto(id);
+            updateDto.AddPlace(place.Id);
+            updateDto.AddSku(sku.Id);
+            groupDetail = await SkuGroupRepo.UpdateSkuGroup(id, updateDto);
+            Assert.IsTrue(groupDetail.Skus.Contains(sku), "Re-added sku");
+            Assert.IsTrue(groupDetail.Places.Contains(place), "Re-added place");
+
+
+            // delete group
             var deleteResult = await SkuGroupRepo.DeleteSkuGroup(group.Id);
             Assert.IsTrue(deleteResult, "deleteResult == true");
         }
