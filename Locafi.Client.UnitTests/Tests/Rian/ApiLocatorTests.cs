695507cc3382767a1bf481858bb48d19458d84cf
Rian Finnegan <rfinnegan@ramp.com.au>
2015-11-24 11:23:55 +1100

    added API locator for devices

--- Locafi.Client.UnitTests/Tests/Rian/ApiLocatorTests.cs
+++ Locafi.Client.UnitTests/Tests/Rian/ApiLocatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,12 +9,32 @@ namespace Locafi.Client.UnitTests.Tests.Rian
     [TestClass]
     public class ApiLocatorTests
     {
+        private const string KnownEmailAddress = "admin@ramp.com.au";
+        private const string KnownDeviceSerialNumber = "123";
+
         [TestMethod]
         public async Task ApiLocator_GetBaseUri()
         {
-            var result = await LocafiApiLocator.GetApiBaseUrl("admin@ramp.com.au"); // this should have an API assocaited with it
+            var result = await LocafiApiLocator.GetApiBaseUrl(KnownEmailAddress); // this should have an API assocaited with it
             Assert.IsInstanceOfType(result, typeof(IDictionary<string,string>));
             Assert.IsTrue(result.Count > 0); // we got at least one URI
+
+            result = await LocafiApiLocator.GetApiBaseUrl(Guid.NewGuid().ToString()); // for an unknown user
+            Assert.IsTrue(result.Keys.Count > 0, "At least the public API should be returned");
+        }
+
+
+        [TestMethod]
+        public async Task ApiLocator_GetBaseUriForDevice()
+        {
+            var result = await LocafiApiLocator.GetApiBaseUrlForDevice(KnownDeviceSerialNumber);
+            Assert.IsNotNull(result, "Result was null when getting known serial number");
+            Assert.IsInstanceOfType(result, typeof(string));
+            var uri = new Uri(result); // to check is valid URI
+
+
+            result = await LocafiApiLocator.GetApiBaseUrlForDevice(Guid.NewGuid().ToString()); // unknown device
+            Assert.IsNull(result);
         }
     }
 }
--- Locafi.Client/LocafiApiLocator.cs
+++ Locafi.Client/LocafiApiLocator.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -20,6 +22,33 @@ namespace Locafi.Client
             var serverMessage = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<Dictionary<string,string>>(serverMessage);
+        }
+
+        public static async Task<string> GetApiBaseUrlForDevice(string serialNumber)
+        {
+            var path = $"{ApiLocatorUrl}/{serialNumber}";
+            var message = new HttpRequestMessage(HttpMethod.Get, path);
+
+            var client = new HttpClient();
+            var response = await client.SendAsync(message);
+            if (!response.IsSuccessStatusCode) return null;
+            var serverMessage = await response.Content.ReadAsStringAsync();
+            try
+            {
+                var record = JsonConvert.DeserializeObject<ApiRecord>(serverMessage);
+                return record.Url;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                return null;
+            }
+        }
+
+        private class ApiRecord
+        {
+            public string Id { get; set; }
+            public string Url { get; set; }
         }
     }
 }
