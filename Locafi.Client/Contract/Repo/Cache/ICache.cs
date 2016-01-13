eafb809f76d197557a8357cb21432ee2e8279fe5
Anthony Zeater <azeater@ramp.com.au>
2016-01-14 10:19:35 +1100

    Modified CopyCache to return IList as well as take a nullable int to return certain number of items.

--- Locafi.Client/Contract/Repo/Cache/ICache.cs
+++ Locafi.Client/Contract/Repo/Cache/ICache.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Locafi.Client.Contract.Repo.Cache
 {
     public interface ICache<T>
     {
         void Push(ICachedEntity<T> entity);
-        IEnumerable<ICachedEntity<T>> CopyCache();
+        IList<ICachedEntity<T>> CopyCache(int? maxCopy = null);
         void Remove(string id);
     }
 }
