8903e1509625c079a8c6c3cece396614d802a3e9
Anthony Zeater <azeater@ramp.com.au>
2016-04-19 15:32:38 +1000

    Added FileUploadDto and operation enum

---
+++ Locafi.Client.Model/Dto/FileUpload/FileUploadDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Locafi.Client.Model.Enums;
+
+namespace Locafi.Client.Model.Dto.FileUpload
+{
+    public class FileUploadDto
+    {
+        public List<Dictionary<string, string>> Entities { get; set; }
+
+        public FileUploadOperation Operation { get; set; }
+    }
+}
---
+++ Locafi.Client.Model/Enums/FileUploadOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locafi.Client.Model.Enums
+{
+    public enum FileUploadOperation
+    {
+        Create = 0,
+        Update,
+        CreateAndUpdate,
+    }
+}
