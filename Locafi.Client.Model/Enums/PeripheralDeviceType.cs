0dad4c886ae44399ff369bf0e002eabb7b7e3dcb
Anthony Zeater <azeater@ramp.com.au>
2016-05-16 12:36:13 +1000

    Added Cs203 reader enum.

--- Locafi.Client.Model/Enums/PeripheralDeviceType.cs
+++ Locafi.Client.Model/Enums/PeripheralDeviceType.cs
@@ -11,6 +11,7 @@ namespace Locafi.Client.Model.Enums
         SpeedwayR220 = 1,
         SpeedwayR420 = 2,
         Barionet50 = 3,
-        ZebraMotorola = 4
+        ZebraMotorola = 4,
+        Cs203 = 5
     }
 }
