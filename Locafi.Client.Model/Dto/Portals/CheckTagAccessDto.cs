69a7f2d8b7954ea39f247bb168af64886f39e5c8
Anthony Zeater <azeater@ramp.com.au>
2016-07-26 10:07:31 +1000

    Added new DTOs for checking if access is allowed.

---
+++ Locafi.Client.Model/Dto/Portals/CheckTagAccessDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locafi.Client.Model.Dto.Portals
+{
+    public class CheckTagAccessDto
+    {
+        public Guid PlaceId { get; set; }
+        public string TagNumber { get; set; }
+    }
+}
---
+++ Locafi.Client.Model/Dto/Portals/TagAccessResultDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locafi.Client.Model.Dto.Portals
+{
+    class TagAccessResultDto
+    {
+        public bool AllowAccess { get; set; }
+        public string PersonName { get; set; }
+        public Guid? SecurityGroupId { get; set; }
+        public string SecurityGroup { get; set; }
+        public string Message { get; set; }
+
+        public TagAccessResultDto()
+        {
+            AllowAccess = false;
+            SecurityGroupId = null;
+        }
+    }
+}
