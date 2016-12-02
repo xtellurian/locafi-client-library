c70c977bd2599da6211e75de6748b4190d23ec44
Anthony Zeater <azeater@ramp.com.au>
2016-12-02 16:16:49 +1100

    Missing integrationdto

---
+++ Locafi.Client.Model/Dto/OutboundIntegrations/IntegrationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locafi.Client.Model.Dto.OutboundIntegrations
+{
+    public class IntegrationDto
+    {
+        public IList<Guid> ListenerAgents { get; set; }
+
+        public string NotificationType { get; set; }
+
+        public object Payload { get; set; }
+    }
+}
